5c5e53d59fc11e7f588972db05905cd6b4d040d9
Ian Unruh <ianunruh@gmail.com>
2013-11-05 16:31:25 -0600

    Implemented PushConfiguration task executor
    Needs a little more love, but it works for our current needs

--- lib/ancor/tasks/push_configuration.rb
+++ lib/ancor/tasks/push_configuration.rb
@@ -1,8 +1,26 @@
 module Ancor
   module Tasks
     class PushConfiguration < BaseExecutor
+      include Conductor::ClientUtil
+
       def perform(instance_id)
-        puts "Pushing configuration to instance #{instance_id}"
+        instance = Instance.find instance_id
+
+        unless context[:ran]
+          puppet_client = rpc_client :puppet, timeout: 3
+          puppet_client.identity_filter instance.name
+
+          client_sync {
+            puppet_client.runonce
+          }
+
+          context[:ran] = true
+
+          create_wait_handle(:run_completed, instance_id: instance.id)
+
+          return false
+        end
+
         return true
       end
     end # PushConfiguration
