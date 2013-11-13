2bf5d3e13e58af67b9cf759e4a8272ea03eae2f7
Ian Unruh <ianunruh@gmail.com>
2013-11-13 11:52:35 -0600

    Removed ScenarioStage
    Moved profile to Scenario, removed data

--- app/models/scenario.rb
+++ app/models/scenario.rb
@@ -5,15 +5,7 @@ class Scenario
   field :description, type: String
   field :slug, type: Symbol
 
-  field :data, type: String
+  field :profile, type: String
 
   belongs_to :role
-
-  embeds_many :stages, class_name: "ScenarioStage"
-
-  def stage(slug)
-    stages.find { |stage|
-      stage.slug == slug
-    }
-  end
 end
