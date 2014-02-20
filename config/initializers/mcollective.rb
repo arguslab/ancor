b1c1d596b970eed70d5da2244074422a874c0737
Ian Unruh <ianunruh@gmail.com>
2014-02-20 11:42:59 -0600

    Switched to MCollective fork, fixes libdir issue

--- config/initializers/mcollective.rb
+++ config/initializers/mcollective.rb
@@ -1,16 +1,14 @@
+conf_path = Rails.root.join('config', 'mcollective', 'client.conf')
+lib_path = Rails.root.join('lib')
+
+unless Dir.exist?(File.join(lib_path, 'mcollective'))
+  raise 'Portable MCollective has not been installed. Run `bin/setup-mcollective` and try again.'
+end
+
 require 'mcollective'
 
 config = MCollective::Config.instance
 unless config.configured
-  conf_path = Rails.root.join('config', 'mcollective', 'client.conf')
-  lib_path = Rails.root.join('lib')
-
-  unless Dir.exist?(File.join(lib_path, 'mcollective'))
-    raise 'Portable MCollective has not been installed. Run `bin/setup-mcollective` and try again.'
-  end
-
   config.loadconfig conf_path
-  # getting pretty tired of your shit, mcollective
-  # https://github.com/puppetlabs/marionette-collective/blob/2.4.x/lib/mcollective/config.rb#L128
-  # config.libdir << lib_path
+  config.libdir << lib_path
 end
