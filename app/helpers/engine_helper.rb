d456a38c0e9103f2e51a0f796932ffd0d4f0fe39
Ian Unruh <ianunruh@gmail.com>
2014-02-27 14:46:49 -0600

    Added generate_user_data to EngineHelper

--- app/helpers/engine_helper.rb
+++ app/helpers/engine_helper.rb
@@ -43,6 +43,25 @@ module EngineHelper
     }
   end
 
+  # Generates user data using the Ubuntu Quantal template and some default
+  # values
+  #
+  # @return [String]
+  def generate_user_data
+    config = Ancor.config
+
+    template = ERB.new(
+      File.read(File.expand_path('spec/config/ubuntu-precise.sh.erb', Rails.root))
+    )
+    mco_template = ERB.new(
+      File.read(File.expand_path('spec/config/mcollective/server.cfg.erb', Rails.root))
+    )
+
+    mcollective_server_config = mco_template.result(binding)
+
+    template.result(binding)
+  end
+
   def openstack_config
     @openstack_config ||= Ancor.config[:openstack]
   end
