b8ab3ded33eb7bfa84b981e696369e0be8a1ae32
Ian Unruh <ianunruh@gmail.com>
2014-03-14 15:32:52 -0500

    Added GNU GPL v3 license to project

--- lib/ancor/provider/openstack/instance_service.rb
+++ lib/ancor/provider/openstack/instance_service.rb
@@ -1,3 +1,20 @@
+# Author: Ian Unruh, Alexandru G. Bardas
+# Copyright (C) 2013-2014 Argus Cybersecurity Lab, Kansas State University
+#
+# This file is part of ANCOR.
+#
+# ANCOR is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ANCOR is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with ANCOR.  If not, see <http://www.gnu.org/licenses/>.
 module Ancor
   module Provider
     class OpenStackInstanceService < InstanceService
