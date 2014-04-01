92bcd640541a395d0b6121ea7e686f0b24bc57e2
Ian Unruh <ianunruh@gmail.com>
2014-04-01 12:57:25 -0500

    Moved CORS options to development config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,5 +22,9 @@ Ancor::Application.configure do
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
 
-
+  config.action_dispatch.default_headers = {
+    'X-Content-Type-Options' => 'nosniff',
+    'Access-Control-Allow-Origin' => '*',
+    'Access-Control-Allow-Methods' => 'GET, POST, PUT, PATCH, DELETE, OPTIONS'
+  }
 end
