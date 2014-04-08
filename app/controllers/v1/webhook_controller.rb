24c4449c81532f0caf63fb1c683725045fb02183
Ian Unruh <ianunruh@gmail.com>
2014-04-08 10:57:53 -0500

    Refactored webhook controller

--- app/controllers/v1/webhook_controller.rb
+++ app/controllers/v1/webhook_controller.rb
@@ -17,38 +17,22 @@
 # along with ANCOR.  If not, see <http://www.gnu.org/licenses/>.
 module V1
   class WebhookController < ApplicationController
-    def mcollective
-      body = request.body.read
-      meta = YAML.load body
-
-      host = meta[:identity].split('.')[0]
-      instance = Instance.where(name: host).first
-
-      if instance
-        process_wait_handles :instance_registered, instance.id
-      else
-        # TODO Log missing instances
-      end
-
-      render nothing: true, status: 200
-    end
-
     def puppet
-      body = request.body.read
-      transaction = YAML.load body
+      report = YAML.load(request.body)
 
-      # Don't care about the domain name (for now)
-      host = transaction.host.split('.')[0]
-      instance = Instance.where(name: host).first
+      instance_name, _ = report.host.split('.')
+      instance = Instance.find_by(name: instance_name)
 
-      if instance
-        if transaction.status == 'failed'
-          process_wait_handles :run_failed, instance.id
-        else
-          process_wait_handles :run_completed, instance.id
-        end
+      model = instance.puppet_reports.create!(
+        transaction_uuid: report.transaction_uuid,
+        started_at: report.time,
+        status: report.status,
+      )
+
+      if model.success?
+        process_wait_handles(:run_completed, instance)
       else
-        # TODO Log missing instances
+        process_wait_handles(:run_failed, instance)
       end
 
       render nothing: true, status: 200
@@ -56,9 +40,9 @@ module V1
 
     private
 
-    def process_wait_handles(type, instance_id)
-      WaitHandle.each_task(type, instance_id: instance_id) do |id|
-        Ancor::TaskWorker.perform_async id
+    def process_wait_handles(type, instance)
+      WaitHandle.each_task(type, instance_id: instance.id) do |id|
+        Ancor::TaskWorker.perform_async(id)
       end
     end
   end # WebhookController
